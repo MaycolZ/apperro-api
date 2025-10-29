2fcad0ad2291e5a81e42b9ac2bbcddd662f50f25
Maycol Zincker <zincker2000@gmail.com>
2025-10-29 02:18:38 -0300

    signin y signup añadidos

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+# Nueva Vista para la página de inicio
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class SignUpView(CreateView):
+    # Usa el formulario integrado de creación de usuarios de Django
+    form_class = UserCreationForm
+
+    # Redirige al usuario a la página de login al completar el registro
+    success_url = reverse_lazy("login")
+
+    # Especifica el nombre del archivo de plantilla (template)
+    template_name = "registration/signup.html"
