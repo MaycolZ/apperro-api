8f5ea07b51ba9602eb6d32a341811d3e68fc0dfb
Maycol Zincker <zincker2000@gmail.com>
2025-11-20 01:21:28 -0300

    feat(auth.service): implementar logica de signup y signin con jwt y bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async signup(data) {
+    const user = await this.usersService.createUser(data);
+    return { message: 'Usuario creado', user };
+  }
+
+  async signup(data) {
+    const user = await this.usersService.createUser(data);
+    return { message: 'Usuario creado', user };
+  }
+
+  async signin(data) {
+    const user = await this.usersService.findByEmail(data.email);
+    if (!user) throw new UnauthorizedException('Credenciales incorrectas');
+
+    const isMatch = await bcrypt.compare(data.password, user.password);
+    if (!isMatch) throw new UnauthorizedException('Credenciales incorrectas');
+
+    const token = await this.jwtService.signAsync({
+      sub: user.id,
+      email: user.email,
+    });
+
+    return { message: 'Login correcto', token };
+  }
+  
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = []
+
+    async createUser(data) {
+        const hashed = await bcrypt.hash(data.password, 10)
+
+        const newUser = {
+            id: this.users.length + 1,
+            name: data.name,
+            email: data.email,
+            password: hashed
+        }
+    
+        this.users.push(newUser)
+        return newUser
+    }
+
+    async findByEmail(email: String) {
+        return this.users.find(u => u.email === email)
+    }
+
+    async findAll() {
+        return this.users
+    }
+}
